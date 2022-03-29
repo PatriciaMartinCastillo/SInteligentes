6eedff31e86fd445105ce1ea7584666211876726
jesuscestino <jesuscestino@uma.es>
2022-03-29 16:01:37 +0200

    Metodos nuevos y codigo depurado

--- codigo/src/Main/Laberinto.java
+++ codigo/src/Main/Laberinto.java
@@ -11,7 +11,6 @@ public class Laberinto {
 	private int dimensionY;
 	private int iniX, iniY, objX, objY;
 	private int prb;
-	
 	private Configuracion cfg;
 	Boolean solucionable;
 
@@ -19,140 +18,59 @@ public class Laberinto {
 	public Laberinto (int dimensionX, int dimensionY, int prob){
 		
 		
-
+		this.dimensionX = dimensionX;
+		this.dimensionY = dimensionY;
 		matriz= new char[dimensionX][dimensionY];
 		prb = prob;
 		
+	}
+		
+		public void generarLaberinto(){
 
-		
-		for(int i=0;i<dimensionX;i++) {
-			for(int j=0;j<dimensionY;j++) {
-				matriz[i][j]= ' ';
-			}
-		}
-		
-		
-	// GENERAR LABERINTO
-		Random ram = new Random();		
-
-		// ram.setSeed(cfg._semilla);		
-	
-		int control=0;
-		
-		int pos1,pos2;
-		
-		
-		//Obstaculos
-		
-		int tope=(prob/100)*(dimensionX*dimensionY);
-				
-		while(control<tope) {
-					
-			//Creo posiciones aleatorias
-				
-			pos1=ram.nextInt(dimensionX);
-			pos2=ram.nextInt(dimensionY);
-			
-			//Compruebo que en esa posicion no se encuentre otro obstaculo
-					
-				if(matriz[pos1][pos2] != '*') {
-							
-					matriz[pos1][pos2] = '*';
-					control++;
+			for(int i=0;i<dimensionX;i++) {
+				for(int j=0;j<dimensionY;j++) {
+					matriz[i][j]= ' ';
 				}
-			
-		}
-				
-				
-			//Inicial	
-				
+			}
+			Random ram = new Random();		
+
+			// ram.setSeed(semilla);		
 		
-		control=0;
-				
-		while(control<1) {
-			
-			//Posicion aleatoria
+			int control=0;
+
+			//Inicio
 			iniX=ram.nextInt(dimensionX);
 			iniY=ram.nextInt(dimensionY);
-			
-			//Es obstaculo
-			
-			if(matriz[iniX][iniY] != '*') {
-				matriz[iniX][iniY] = 'I';
-				control++;
+			matriz[iniX][iniY] = 'I';
+
+			//Objetivo
+			while(control<1){
+				objX=ram.nextInt(dimensionX);
+				objY=ram.nextInt(dimensionY);
+				if(matriz[objX][objY] != 'I') {
+					matriz[objX][objY] = 'G';
+					control++;
+				}
 			}
-			
+
+			//Obstaculos
+			//Si no es inicio, no es fin y no entra en probabilidad entonces es obstaculo
+			for(int i=0;i<dimensionX;i++) {
+				for(int j=0;j<dimensionY;j++) {
+					if(matriz[i][j]!='G' && matriz[i][j]!='I' && ram.nextInt(100)<prb){
+						matriz[i][j]= '*';
+					}
+				}
+			}		
+		
 		}
-		
-		
-		//objetivo
-		
-		control=0;
-		
-		while(control<1) {
-			
-			//Posicion aleatoria
-			objX=ram.nextInt(dimensionX);
-			objY=ram.nextInt(dimensionY);
-			
-			//Es obstaculo
-			
-			if(matriz[objX][objY] != '*' && matriz[objX][objY] != 'I') {
-				matriz[objX][objY] = 'G';
-				control++;
+
+	public void mostrarlaberinto(){
+		for(int i=0;i<dimensionX;i++) {
+			System.out.println("");
+			for(int j=0;j<dimensionY;j++) {
+				System.out.print(matriz[dimensionX][dimensionY]);
 			}
-			
 		}
-		
-	
 	}
-
-    
-
-	public Nodo[][] getmatriz() {
-		return matriz;
-	}
-
-
-
-	public int[] getinicio() {
-		return inicio;
-	}
-
-
-
-	public int[] getobjetivo() {
-		return objetivo;
-	}
-
-
-
-	public int[][] getObstaculos() {
-		return obstaculos;
-	}
-
-public Nodo getInicio() {
-		
-		return new Nodo(inicio[0],inicio[1]);
-	}
-	
-	public Nodo getobjetivo() {
-		
-		return new Nodo(objetivo[0],objetivo[1]);
-	}
-
-
-
-	public Configuracion getCfg() {
-		return cfg;
-	}
-
-
-
-	public void setCfg(Configuracion cfg) {
-		this.cfg = cfg;
-	}
-
-	
-
 }
---
+++ codigo/src/Main/Main.java
@@ -0,0 +1,7 @@
+package Main;
+
+
+public public static void main(String[] args) {
+    Laberinto lab = new Laberinto(60,80,30);
+    lab.mostrarlaberinto();    
+}
